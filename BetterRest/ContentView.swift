a8120328ad58ec04239013ca1509c78a4b66133f
Chris Wu <31398736+shiftingsand@users.noreply.github.com>
2020-05-01 16:00:46 -0400

    Day 28 - Challenge 3. "Change the user interface so that it always shows their recommended bedtime using a nice and large font. You should be able to remove the “Calculate” button entirely."

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -17,9 +17,13 @@ struct ContentView: View {
     @State private var showingAlert = false
     
     var body: some View {
-        
         NavigationView {
             Form {
+                Section(header: Text("Current recommended bedtime").font(.headline)) {
+                    Text("\(getFinalUserMsg())")
+                        .font(.headline)
+                }
+                
                 Section(header: Text("When do you want to wake up?").font(.headline)) {
                     
                     DatePicker("Please enter a time", selection: $wakeUP, displayedComponents: .hourAndMinute)
@@ -40,17 +44,13 @@ struct ContentView: View {
                         ForEach(1..<21) { indexer in
                             Text("\(indexer)")
                         }
-                    }.labelsHidden()
-                }
-            } // Form
-                .navigationBarTitle("BetterRest")
-                .navigationBarItems(trailing:
-                    Button(action: calculateButton) {
-                        Text("Calculate")
                     }
-            )
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                    .labelsHidden()
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
         }
     }
@@ -60,6 +60,34 @@ struct ContentView: View {
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components) ?? Date()
+    }
+
+    //  Day 28 - Challenge 3.
+    // "Change the user interface so that it always shows their
+    //  recommended bedtime using a nice and large font. You
+    //  should be able to remove the “Calculate” button entirely.
+    func getFinalUserMsg() -> String {
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUP)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        let userMsg : String
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUP - prediction.actualSleep
+
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            userMsg = formatter.string(from: sleepTime)
+        } catch {
+            userMsg = "Error with calculation"
+        }
+        
+        return userMsg
     }
     
     func calculateButton() {
